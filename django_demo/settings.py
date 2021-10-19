73ab725b10e783f2e383a3c457c9fe399af0fa8e
aquashdw <aquashdw@gmail.com>
2021-10-19 15:16:58 +0900

    add apps to django service

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "landing",
+    "blog",
+    "shopping",
+    "payment",
+    "local_shops",
 ]
 
 MIDDLEWARE = [
