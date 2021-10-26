3e3e0d110c8a9eba19cc75b05bb2af64c61f056b
aquashdw <aquashdw@gmail.com>
2021-10-26 12:27:33 +0900

    rework blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from blog import views
 
 app_name = "blog"
 urlpatterns = [
-    path("home/", views.blog_home, name="home"),
-    path("post-view/<int:post_id>/", views.blog_post_view, name="post-view"),
-    path("post-write/", views.blog_post_write, name="post-write"),
-    path("post-edit/<int:post_id>/", views.blog_post_edit, name="post-edit"),
-    path("post-delete/<int:post_id>/", views.blog_post_delete, name="post-delete")
+    path("<int:blog_id>/", views.shop_blog_home, name="home"),
+    path("<int:blog_id>/post/<int:post_id>/", views.shop_blog_post, name="post"),
+    path("<int:blog_id>/post/write/", views.shop_blog_post_write, name="write"),
+    path("<int:blog_id>/post/<int:post_id>/edit/", views.shop_blog_post_edit, name="edit"),
+    path("<int:blog_id>/post/<int:post_id>/delete/", views.shop_blog_post_delete, name="delete")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,65 +1,21 @@
-from django.shortcuts import render, reverse
-from django.http import HttpResponseRedirect
-
-from blog.models import Post
+from django.shortcuts import render
 
 
-def blog_home(request):
-    post_list = Post.objects.all()
-    context = {
-        "post_list": post_list
-    }
-    return render(request, "blog/blog-home.html", context)
+def shop_blog_home(request, blog_id):
+    return render(request, "blog/blog-home.html")
 
 
-def blog_post_view(request, post_id):
-    post = Post.objects.get(id=post_id)
-    context = {
-        "post": post
-    }
-    return render(request, "blog/post.html", context)
+def shop_blog_post(request, blog_id, post_id):
+    return render(request, "blog/blog-post.html")
 
 
-def blog_post_write(request):
-    if request.method == "POST":
-        new_post = Post()
-        new_post.title = request.POST["title"]
-        new_post.content = request.POST["content"]
-        if request.FILES.get("image"):
-            new_post.header_image = request.FILES["image"]
-        new_post.likes = 0
-        new_post.save()
-        return HttpResponseRedirect(reverse("blog:home"))
-
-    return render(request, "blog/create.html")
+def shop_blog_post_write(request, blog_id):
+    return render(request, "blog/blog-post-write.html")
 
 
-def blog_post_edit(request, post_id):
-    target_post = Post.objects.get(id=post_id)
-
-    if request.method == "GET":
-        context = {
-            "post": target_post
-        }
-        return render(request, "blog/edit.html", context)
-    elif request.method == "POST":
-        target_post.title = request.POST["title"]
-        target_post.content = request.POST["content"]
-        if request.FILES.get("image"):
-            target_post.header_image = request.FILES["image"]
-
-        target_post.save()
-
-        return HttpResponseRedirect(f"/blog/post-view/{post_id}/")
+def shop_blog_post_edit(request, blog_id, post_id):
+    return render(request, "blog/blog-post-edit.html")
 
 
-def blog_post_delete(request, post_id):
-    target_post = Post.objects.get(id=post_id)
-    if request.method == "GET":
-        context = {
-            "post": target_post
-        }
-        return render(request, "blog/delete.html", context)
-    elif request.method == "POST":
-        target_post.delete()
-        return HttpResponseRedirect("/blog/home/")
+def shop_blog_post_delete(request, blog_id, post_id):
+    return render(request, "blog/blog-post-delete.html")
