36ebbef16e9bedcb62de23d3188c876479c87368
aquashdw <aquashdw@gmail.com>
2021-10-26 11:09:20 +0900

    rework shop

--- shopping/views.py
+++ shopping/views.py
@@ -11,3 +11,39 @@ def shopping_admin(request):
 
 def shopping_item(request):
     return render(request, "shopping/item.html")
+
+
+# start here
+def shop_home(request, shop_id):
+    context = {
+
+    }
+    return render(request, "shopping/index.html", context)
+
+
+def shop_item(request, shop_id, item_id):
+    context = {
+
+    }
+    return render(request, "shopping/item.html", context)
+
+
+def shop_admin(request, shop_id):
+    context = {
+
+    }
+    return render(request, "shopping/admin.html", context)
+
+
+def shop_admin_add_item(request, shop_id):
+    context = {
+
+    }
+    return render(request, "shopping/admin-add.html", context)
+
+
+def shop_admin_edit(request, shop_id, item_id):
+    context = {
+
+    }
+    return render(request, "shopping/admin-edit.html", context)
