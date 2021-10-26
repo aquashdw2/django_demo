16a419573622fecf07d734185dd5f68b31db29cf
aquashdw <aquashdw@gmail.com>
2021-10-26 12:32:51 +0900

    rename htmls

--- shopping/views.py
+++ shopping/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 
 
 def shopping_home(request):
-    return render(request, "shopping/index.html")
+    return render(request, "shopping/shop-home.html")
 
 
 def shopping_admin(request):
-    return render(request, "shopping/admin.html")
+    return render(request, "shopping/shop-admin.html")
 
 
 def shopping_item(request):
-    return render(request, "shopping/item.html")
+    return render(request, "shopping/shop-item.html")
 
 
 # start here
@@ -18,32 +18,32 @@ def shop_home(request, shop_id):
     context = {
 
     }
-    return render(request, "shopping/index.html", context)
+    return render(request, "shopping/shop-home.html", context)
 
 
 def shop_item(request, shop_id, item_id):
     context = {
 
     }
-    return render(request, "shopping/item.html", context)
+    return render(request, "shopping/shop-item.html", context)
 
 
 def shop_admin(request, shop_id):
     context = {
 
     }
-    return render(request, "shopping/admin.html", context)
+    return render(request, "shopping/shop-admin.html", context)
 
 
 def shop_admin_add_item(request, shop_id):
     context = {
 
     }
-    return render(request, "shopping/admin-add.html", context)
+    return render(request, "shopping/shop-admin-add.html", context)
 
 
 def shop_admin_edit(request, shop_id, item_id):
     context = {
 
     }
-    return render(request, "shopping/admin-edit.html", context)
+    return render(request, "shopping/shop-admin-edit.html", context)
