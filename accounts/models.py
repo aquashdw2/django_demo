f695f87add6a84c663f52cb687d7351720d4f5cb
aquashdw <aquashdw@gmail.com>
2021-10-27 16:02:21 +0900

    make info, area, shop

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from local_shops.models import Area
+
+
+class Info(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
+    is_shop_owner = models.BooleanField(default=False)
--- shopping/models.py
+++ shopping/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+from local_shops.models import Area
+
+
+class Shop(models.Model):
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
