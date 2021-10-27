c3d3bdc4acc3e6ebed2e27c45d41ebbbda7cf60a
aquashdw <aquashdw@gmail.com>
2021-10-27 16:19:56 +0900

    add shops and visited

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from local_shops.models import Area
+from shopping.models import Shop
 
 
 class Info(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
     is_shop_owner = models.BooleanField(default=False)
+
+
+class Visited(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.SET_NULL, null=True)
